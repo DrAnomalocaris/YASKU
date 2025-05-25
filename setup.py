7937b0567557e4f5c2b16329c9094c4f6b5a0011
Albert Lahat <lahat.albert@gmail.com>
2025-05-25 14:49:47 -0400

    updated version, small bug

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="yasku",
-    version="0.1.0",
+    version="0.1.1",
     description="Yet Another Science Keppy Upy - Keep up to date with science fields via PubMed and Discord.",
     author="Albert Lahat",
     packages=find_packages(),
@@ -24,4 +27,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
